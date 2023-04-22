33b4a871537041a45a814c8de408e2e5a65c48bd
rood8592 <rrodasgaleano@frba.utn.edu.ar>
2023-04-21 22:00:51 -0300

    creado el archivo hello.c y su salida fue redireccionada a output.txt

---
+++ 00-CHelloworld/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define cte valor 
+
+
+int main (int argc,char* argv[])
+{
+    char hola[15]="Hello, World!";
+    printf("%s", hola);
+    return 0;
+}
